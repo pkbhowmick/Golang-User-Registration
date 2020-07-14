6ae09a5ac8046f446368b4f6029bc9ba0f736ed2
Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>
2020-07-14 22:40:33 +0600

    Add put,post,delete

--- server.go
+++ server.go
@@ -15,9 +15,29 @@ func get(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte (`{"message":"In get method"}`))
 }
+
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte (`{"message":"In post method"}`))
+}
+func put(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte (`{"message":"In put method"}`))
+}
+func delete(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte (`{"message":"In delete method"}`))
+}
+
 func main(){
 	fmt.Println("Server is running...")
 	http.HandleFunc("/",handler)
-	http.HandleFunc("/get",get)
+	http.HandleFunc("/api/get",get)
+	http.HandleFunc("/api/post",post)
+	http.HandleFunc("/api/put",put)
+	http.HandleFunc("/api/delete",delete)
 	http.ListenAndServe(":8080", nil)
 }
