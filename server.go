2df3459a5f2ca6bade00805dd10d745a0d7060f1
Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>
2020-07-26 00:23:41 +0600

    signup and signin

--- server.go
+++ server.go
@@ -10,6 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
 )
 
 
@@ -21,6 +22,16 @@ type User struct{
 }
 
 var client *mongo.Client
+
+func getHash(pwd []byte) string {
+    
+    hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+    if err != nil {
+        log.Println(err)
+    }
+    return string(hash)
+}
+
 
 func getUsers(response http.ResponseWriter, request *http.Request){
   response.Header().Set("Content-Type","application/json")
@@ -50,28 +61,38 @@ func getUsers(response http.ResponseWriter, request *http.Request){
 
 }
 
-func getUser(response http.ResponseWriter, request *http.Request){
+func userLogin(response http.ResponseWriter, request *http.Request){
   response.Header().Set("Content-Type","application/json")
   var user User
-  params:= mux.Vars(request)
-  email,_:= params["email"]
+  var dbuser User
+  json.NewDecoder(request.Body).Decode(&user)
   collection:= client.Database("GODB").Collection("user")
   ctx,_ := context.WithTimeout(context.Background(),10*time.Second)
-  err:= collection.FindOne(ctx, bson.M{"email":email}).Decode(&user)
+  err:= collection.FindOne(ctx, bson.M{"email":user.Email}).Decode(&dbuser)
 
   if err!=nil{
 	  response.WriteHeader(http.StatusInternalServerError)
 	  response.Write([]byte(`{"message":"`+err.Error()+`"}`))
 	  return
   }
+  userPass:= []byte(user.Password)
+  dbPass:= []byte(dbuser.Password)
 
-  json.NewEncoder(response).Encode(user)
+  passErr:= bcrypt.CompareHashAndPassword(dbPass, userPass)
+
+  if passErr != nil{
+	  log.Println(passErr)
+	  response.Write([]byte(`{"response":"Wrong Password!"}`))
+	  return
+  }
+  response.Write([]byte(`{"token":"JWT token"}`))
 }
 
-func createUser(response http.ResponseWriter, request *http.Request){
+func userSignup(response http.ResponseWriter, request *http.Request){
 	response.Header().Set("Content-Type","application/json")
 	var user User
 	json.NewDecoder(request.Body).Decode(&user)
+	user.Password = getHash([]byte(user.Password))
 	collection := client.Database("GODB").Collection("user")
 	ctx,_ := context.WithTimeout(context.Background(), 10*time.Second)
 	result,_ := collection.InsertOne(ctx,user)
@@ -96,11 +117,11 @@ func main(){
 	client,_= mongo.Connect(ctx,options.Client().ApplyURI("mongodb://localhost:27017"))
 
 	router.HandleFunc("/api/users",getUsers).Methods("GET")
-	router.HandleFunc("/api/user/{email}",getUser).Methods("GET")
-	router.HandleFunc("/api/user",createUser).Methods("POST")
+	router.HandleFunc("/api/user/login",userLogin).Methods("POST")
+	router.HandleFunc("/api/user/signup",userSignup).Methods("POST")
 	router.HandleFunc("/api/user/{email}",updateUser).Methods("PUT")
 	router.HandleFunc("/api/user/{email}",deleteUser).Methods("DELETE")
 
-	http.ListenAndServe(":8080", router)
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
